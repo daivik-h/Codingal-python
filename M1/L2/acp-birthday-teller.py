03878b210a93616cee84ae886f3bda4b3eb88bff
daivik-h <daivikh429@gmail.com>
2026-03-05 17:04:01 +0100

    Birthday's

---
+++ M1/L2/acp-birthday-teller.py
@@ -0,0 +1,30 @@
+name1 = input(f'Enter Your Name Person 1 : ')
+person1age = input(f'Person 1 Enter your age : ')
+person1birthday = input(f' Enter Your Date of Birth : ')
+name2 = input(f'Enter Your Name Person 2 : ')
+person2age = input(f'Person 2 Enter your age : ')
+person2birthday = input(f' Enter Your Date of Birth : ')
+
+name3 = input(f'Enter Your Name Person 3 : ')
+person3age = input(f'Person 3 Enter your age : ')
+person3birthday = input(f' Enter Your Date of Birth : ')
+
+name4 = input(f'Enter Your Name Person 4 : ')
+person4age = input(f'Person 4 Enter your age : ')
+person4birthday = input(f' Enter Your Date of Birth : ')
+
+
+
+
+
+
+
+print(f'Hi {name1} You are {person1age} And you will  have your Birthday on {person1birthday} wuhoo ') 
+
+print(f'Hi {name2} You are {person2age} And you will  have your Birthday on {person2birthday} Yahoo ') 
+
+print(f'Hi {name3} You are {person3age} And you will  have your Birthday on {person3birthday} wuhoo ') 
+
+print(f'Hi {name4} You are {person4age} And you will  have your Birthday on {person4birthday} Yahoo ') 
+
+
