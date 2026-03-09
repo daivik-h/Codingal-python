3589943fdf332c82a4a50e45250b38eb8dec335c
daivik-h <daivikh429@gmail.com>
2026-03-09 17:00:21 +0100

    Convertor

---
+++ M1/L3/acp-conversion.py
@@ -0,0 +1,9 @@
+userinput = input('Enter Any Word and i will convert it into uppper case : ')
+
+uppercaseconverson = userinput.upper()
+
+
+
+
+
+print(f"This is your cnversion : {uppercaseconverson}")
---
+++ M1/L3/acp-typecating.py
@@ -0,0 +1,5 @@
+a = float(4)
+b = int(4.8) 
+c = str(4)
+
+print(f'{a} {b} {c}')
