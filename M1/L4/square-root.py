eee7e03d9a07555bfe6e2bf7c6b7f33414defcb3
daivik-h <daivikh429@gmail.com>
2026-03-12 14:26:00 +0100

    The Squareroot Calculator

---
+++ M1/L4/square-root.py
@@ -0,0 +1,6 @@
+num = int(input('Please only add a whole number i will tell the squareroot of it : '))
+if num < 0: 
+    print('Sorry i cant do whole numbers')
+else:  
+    result = num**0.5
+    print(f'The Squareroot of {num} is {result}')
