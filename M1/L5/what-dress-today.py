67bb39278d02218bbf559cf9610db3058417e1f2
daivik-h <daivikh429@gmail.com>
2026-03-16 18:42:57 +0100

    The BMI Calculator(not Friendly)

--- M1/L5/what-dress-today.py
+++ M1/L5/what-dress-today.py
@@ -1,15 +1,18 @@
-input = int(input('Hey Enter The Temerature : '))
+temperature = int(input('Hey Enter The Temerature : '))
  
-temperature = input
 
-if temperature <= 0:
+
+if temperature < 0:
     print(f'The Temperature is {temperature} thats frezing you better take a good sweater or hoddie and remember the basse layer and jacket 🥶')
 
-elif temperature > 0 :
+elif temperature > 0 and temperature <= 10 :
     print(f'The Temperature is {temperature} stil pretty cold you better take a good sweater or hoddie and remember a coat 🤧 ')   
 
-elif temperature > 10 :
+elif temperature > 10 and temperature <= 16 :
     print(f'The Temperature is {temperature} thats perfect a shirt with a pull over should be fine 😊')    
 
 elif temperature > 16  :
-    print(f'The Temperature is {temperature} thats thats perfect summer temperature take a tshirt and shorts or pant is fine you will sweat anyway 😁😎🥵')    
+    print(f'The Temperature is {temperature} thats thats perfect summer temperature take a tshirt and shorts or pant is fine you will sweat anyway 😁😎🥵') 
+
+else :
+    print('invalid temparature')       
