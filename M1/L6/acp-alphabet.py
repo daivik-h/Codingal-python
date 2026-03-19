5dc8dd056da5fa09d88621385c1565ef5f915458
daivik-h <daivikh429@gmail.com>
2026-03-19 15:08:40 +0100

    The Alpha Checker

---
+++ M1/L6/acp-alphabet.py
@@ -0,0 +1,9 @@
+user_input = input('Enter a charecter : ')
+
+if user_input.isalpha():
+    print(f'{user_input} Is in the alphabet or it is a word ! ')
+
+else:
+    print(f'{user_input} is not in the alphabet') 
+    
+       
