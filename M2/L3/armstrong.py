4a2e189e72bcd871b154cbc890e32f5806bdcb77
daivik-h <daivikh429@gmail.com>
2026-03-27 18:44:33 +0100

    While Loop

---
+++ M2/L3/armstrong.py
@@ -0,0 +1,20 @@
+def is_armstrong(num):
+    sum = 0
+    temp = num
+    while temp > 0:
+        digit = temp%10
+        sum += digit**3
+        temp //=10
+    if sum == num:
+        return True
+
+    else:
+        return False        
+
+user_input = int(input('Enter Any Number And i whil check if it is a Armstrong Number: '))
+
+if is_armstrong(user_input):
+    print(f'{user_input} is a Amrstrong number😱')        
+
+else:
+    print(f'Sorry {user_input} Is not a armstrong number 😏')            
