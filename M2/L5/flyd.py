88b8a268d5f818f8cc1197bc35f5ed0aba22db6f
daivik-h <daivikh429@gmail.com>
2026-04-06 18:45:15 +0200

    The pyramids of Arun

---
+++ M2/L5/flyd.py
@@ -0,0 +1,8 @@
+print('The diomond pyramid (please enter the number of rows)')
+n = int(input('here:'))
+
+for i in range(n): 
+    for j in range(i+1):
+    
+        print( 1, end="")
+    print()
